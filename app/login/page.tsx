43f3af584ffbcfae3d20fb821d24ce270863fe2a
ZidanAK22 <zidanazrak@gmail.com>
2024-12-22 10:44:00 +0700

    supabase auth integrated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,22 @@
 'use client'
+
 import { useState } from "react";
 import { Inter } from "next/font/google";
-import { Input, Link } from "@nextui-org/react";
+import { Input, Link, Button } from "@nextui-org/react";
 import { EyeFilledIcon } from "../ui/icon/EyeFilledIcon";
 import { EyeSlashFilledIcon } from "../ui/icon/EyeSlashFilledIcon";
+import { login, signup } from "./actions";
 
 const inter = Inter({ subsets: ["latin"] });
+
+function consolePrint(formData: FormData) {
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
+
+    console.log(data)
+}
 
 export default function LoginPage() {
     const [isVisible, setIsVisible] = useState(false);
@@ -22,39 +33,53 @@ export default function LoginPage() {
 
             {/* Input Form */}
             <div className="min-w-full bg-secondary flex justify-center items-center rounded-xl mt-8 text-[#000000] px-6 py-12">
-                <div className="flex flex-col gap-6 max-w-xl w-full">
+                <form className="flex flex-col gap-6 max-w-xl w-full">
                     <Input
+                        id="email"
                         type="email"
                         variant="bordered"
                         label="Email"
                         placeholder="Masukkan Email Disini"
+                        required
                     />
                     <div className="flex flex-col text-right">
-                    <Input
-                        radius="lg"
-                        variant="bordered"
-                        label="Password"
-                        endContent={
-                            <button
-                                className="focus:outline-none flex items-center"
-                                type="button"
-                                onClick={toggleVisibility}
-                                aria-label="toggle password visibility"
-                            >
-                                {isVisible ? (                                    
-                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none mt-1" />                                    
-                                ) : (
-                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none mt-1" />
-                                )}
-                            </button>
-                        }
-                        type={isVisible ? "text" : "password"}
-                    />
-                    <Link>
-                        Forgot Password?
-                    </Link>
+                        <Input
+                            id="password"
+                            radius="lg"
+                            variant="bordered"
+                            label="Password"
+                            endContent={
+                                <button
+                                    className="focus:outline-none flex items-center"
+                                    type="button"
+                                    onClick={toggleVisibility}
+                                    aria-label="toggle password visibility"
+                                >
+                                    {isVisible ? (
+                                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none mt-1" />
+                                    ) : (
+                                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none mt-1" />
+                                    )}
+                                </button>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            required
+                        />
+                        <Link>
+                            Forgot Password?
+                        </Link>
+
+                        <div className="flex justify-center space-x-4">
+                            <Button formAction={consolePrint} type="submit" className="bg-accent rounded-lg p-4">
+                                Test Data
+                            </Button>
+                            <Button formAction={signup} type="submit" className="bg-accent rounded-lg p-4">
+                                Sign Up
+                            </Button>
+                        </div>
+
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     );
